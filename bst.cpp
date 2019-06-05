27c2467cd5fb8ec029799f90e7ab9305b16075ff
Ferdinand Thomas <ferdinand0022@gmail.com>
2019-06-05 15:52:30 +0530

    bst

---
+++ bst.cpp
@@ -0,0 +1,132 @@
+#include<iostream>
+
+class node{
+public:
+	int data;
+	node *left,*right;
+	node(int data){
+		this->data=data;
+		left=NULL;
+		right=NULL;
+	}
+	~node(){
+		if(!left)
+			delete(left);
+		if(!right)
+			delete(right);
+	}
+};
+
+//Code to display inorder successor of BST... 'Always in sorted order'
+void dispbst(node *root){
+	if(root!=NULL){
+		dispbst(root->left);
+		std::cout<<root->data<<" ";
+		dispbst(root->right);
+	}
+}
+
+//Insertion into BST
+node *insertbst(node *root,int val){
+	if(root==NULL)
+		return (new node(val));
+	if(root->data>val)
+		root->left=insertbst(root->left,val);
+	if(root->data<val)
+		root->right=insertbst(root->right,val);
+	return root;
+}
+
+//Searches and returns a node based on the key value
+node *search(node *root,int key){
+	if(root==NULL||root->data==key)
+		return root;
+	if(root->data<key)
+		return search(root->right,key);
+	return search(root->left,key);
+}
+
+//Utility function for BST deletion
+node *inordersuccessor(node *root){
+	node *temp=root->left==NULL?root:inordersuccessor(root->left);
+	return temp;
+}
+//Delete a node from BST
+node *deletenode(node *root,int val){
+	if(root==NULL){
+		std::cout<<"Element to be deleted not found";
+		return root;
+	}
+	if(root->data > val)
+		root->left=deletenode(root->left,val);
+	if(root->data < val)
+		root->right=deletenode(root->right,val);
+	else{
+		if(root->left==NULL){
+			node * temp= root->right;
+			delete(root);
+			return temp;
+		}
+		else if(root->right==NULL){
+			node * temp=root->left;
+			delete(root);
+			return temp;
+		}
+		else{
+			node *temp= inordersuccessor(root->right);
+			root->data = temp->data;
+			root->right= deletenode(root->right,root->data);
+		}
+	}
+	return root;
+}
+
+
+//Find Kth largrest element from BST
+int kthlargest(node *root,int k){
+	if(root==NULL){
+		return 0;
+	}
+	int r = kthlargest(root->right,k);
+	if(k-(r+1)==0)
+		std::cout<<root->data;
+	int l=kthlargest(root->left,k-(r+1));
+	return l+r+1;
+}
+
+//Find kth smallest element from BST
+int kthsmallest(node *root,int k){
+	if(root==NULL)
+		return 0;
+	int l=kthsmallest(root->left,k);
+	if(k-(l+1)==0)
+		std::cout<<root->data;
+	int r=kthsmallest(root->right,k-(l+1));
+	return l+r+1;
+}
+int main(){	
+	node *root=NULL;
+	root=insertbst(root,1);
+	insertbst(root,16);
+	insertbst(root,21);
+	insertbst(root,10);
+	insertbst(root,40);
+	insertbst(root,25);
+	insertbst(root,15);
+	dispbst(root);
+	deletenode(root,40);
+	std::cout<<"\n";
+	dispbst(root);
+	deletenode(root,16);
+	std::cout<<"\n";
+	dispbst(root);
+	std::cout<<"\n";
+	kthlargest(root,3);
+	std::cout<<"\n";
+	kthlargest(root,1);
+	std::cout<<"\n";
+	kthsmallest(root,3);
+	std::cout<<"\n";
+	kthsmallest(root,2);
+	delete(root);
+}
